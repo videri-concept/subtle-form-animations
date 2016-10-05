69577a90d6a209b219b3b0f067160c4628e524b6
Merlin Schloemer <merlinschloemer@me.com>
2016-10-05 21:12:24 +1300

    set up

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# subtle-form-animations
+subtle-form-animations
+===========================
+
+Small project to help with the animations and ideas behind forms and their animations.
+
+Using the following plugins:
+- SASS Bootstrap 3.3
+- jQuery 1.11.0
+- Modernizer
+- Bootstrap Vaildator
+- HTML5SHIV
+- FontAwesome
+
+You will need Codekit or other SASS compiler to get going
---
+++ assets/javascripts/application.js
@@ -0,0 +1,45 @@
+jQuery(document).ready(function($){
+
+	if( $('.floating-labels').length > 0 ) moveLabel();
+
+	function moveLabel() {
+		var inputFields = $('.floating-labels .flexi-label').next();
+		inputFields.each(function(){
+			var singleInput = $(this);
+			//check if user is filling one of the form fields 
+			checkVal(singleInput);
+			singleInput.on('change keyup', function(){
+				checkVal(singleInput);	
+			});
+		});
+	}
+
+	//check inputs have value
+	function checkVal(inputField) {
+		( inputField.val() === '' ) ? inputField.prev('.flexi-label').removeClass('flexing') : inputField.prev('.flexi-label').addClass('flexing');
+	}
+});
+
+$(document).ready(function() {
+	window.console.log("Mae govannen mellon!");
+
+	$('.btn-submit').on('click', function(e){
+		$(this).addClass('success');
+		$('.form-outer').addClass('success');
+		e.preventDefault();
+	});
+
+	//if search form has focus perform x
+	$('.search-form').focus(function() {
+		$('.search-icon').addClass('expand');
+		$('.search-btn').addClass('expand');
+	});
+
+	//if search form loses focus perform x
+	$('.search-form').blur(function() {
+		$('.search-icon').removeClass('expand');
+		$('.search-btn').removeClass('expand');
+	});
+
+});
+
